047dfb74572f322b94b150aa809c457f0badf516
Marco Serrano <marcoTheWebDev@gmail.com>
2019-09-05 18:04:29 -0500

    Added getter and setter for cells, and a liveNeighbor count function.

--- engine.js
+++ engine.js
@@ -25,6 +25,60 @@ class Board {
         return grid
     }
 
+    setCell(x, y, status) {
+        if(this.grid[x][y] != undefined) {
+            this.grid[x][y] = status
+        }
+        else {
+            console.error("Out of bounds.")
+        }
+    }
+
+    getCell(x, y) {
+        return this.grid[x][y]
+    }
+
+    liveNeighbors(x, y) {
+        let liveNeighbors = 0;
+
+        if(this.getCell((x + this.width - 1) % this.width, (y + this.height + 1) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+
+        if(this.getCell((x + this.width) % this.width, (y + this.height + 1) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+
+        if(this.getCell((x + this.width + 1) % this.width, (y + this.height + 1) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+
+        if(this.getCell((x + this.width + 1) % this.width, (y + this.height) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+
+        if(this.getCell((x + this.width + 1) % this.width, (y + this.height - 1) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+
+        if(this.getCell((x + this.width) % this.width, (y + this.height - 1) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+        if(this.getCell((x + this.width - 1) % this.width, (y + this.height - 1) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+
+        if(this.getCell((x + this.width - 1) % this.width, (y + this.height) % this.height) == ALIVE){
+            liveNeighbors++;
+        }
+
+        return liveNeighbors
+    }
+
+    nextState(x, y) {
+
+    }
+
     printGrid() {
         let str = ''
 
@@ -34,7 +88,6 @@ class Board {
             })
             console.log(str)
             str = ''
-            //console.log('\n')
         }
     }
 }
@@ -42,4 +95,17 @@ class Board {
 let boardy = new Board(10, 8)
 
 boardy.createGrid()
+boardy.setCell(5, 6, ALIVE)
+boardy.setCell(8, 7, ALIVE)
+boardy.setCell(1, 1, ALIVE)
+boardy.setCell(0, 0, ALIVE)
+boardy.setCell(7, 1, ALIVE)
+boardy.setCell(2, 7, ALIVE)
+boardy.setCell(4, 1, ALIVE)
+
 boardy.printGrid()
+
+console.log('0, 2 neighbors: ' + boardy.liveNeighbors(0, 2))
+console.log('2, 0 neighbors: ' + boardy.liveNeighbors(2, 0))
+console.log('0, 1 neighbors: ' + boardy.liveNeighbors(0, 1))
+console.log('4, 4 neighbors: ' + boardy.liveNeighbors(4, 4))
